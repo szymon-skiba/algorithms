2614eeb9dbaba1b143660373206912f24329526c
szymon <skibaexe@gmail.com>
2022-10-18 23:57:43 +0200

    Testy jednostkowe: quickSort,insertionSort,selectionSort.

--- aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/QuickSortTest.java
+++ aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/QuickSortTest.java
@@ -1,4 +1,159 @@
 package pl.edu.pw.ee;
 
+import java.util.Arrays;
+import java.util.Random;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+import pl.edu.pw.ee.services.Sorting;
+
 public class QuickSortTest {
+
+    private Sorting sorter;
+    private Random random;
+    private static final double EPS = 0;
+    private static final long SEED = 173;
+    private static final int TEST_SIZE = 1000;
+
+    private double generateRandomWithSeed() {
+        return (random.nextDouble() * 1000);
+
+    }
+
+    @Before
+    public void setUp() {
+        random = new Random(SEED);
+        sorter = new QuickSort();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkIfNullTest() {
+        //given 
+        double[] nums = null;
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        assert false;
+    }
+
+    @Test
+    public void emptyArrayGiven() {
+        //given 
+        double[] nums = {};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        assertEquals(0, nums.length);
+    }
+
+    @Test
+    public void arrayWithCharsGiven() {
+        //given
+        double[] nums = {'d','b','c','a',1};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        double[] expected = {1,'a','b','c','d'};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void oneElemntArray() {
+        //given
+        double[] nums = {1};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        double[] expected = {1};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void sortedAscArrayGiven() {
+        //given
+        double[] nums = {1, 2, 3, 4, 5};
+
+        //when
+        sorter.sort(nums);
+        
+        //then
+        double[] expected = {1, 2, 3, 4, 5};
+        assertArrayEquals(expected, nums, EPS);
+
+    }
+
+    @Test
+    public void sortedDescArrayGiven() {
+        //given
+        double[] nums = {5, 4, 3, 2, 1};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        double[] expected = {1, 2, 3, 4, 5};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void worstCaseScenarioArrayGiven() {
+        //given 
+        double[] nums = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+        //when 
+        sorter.sort(nums);
+
+        //then 
+        double[] expected = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void bestCaseScenarioArrayGiven() {
+        //given 
+        double[] nums = {6, 1, 2, 5, 4, 3, 8, 9, 10, 7};
+
+        //when 
+        sorter.sort(nums);
+
+        //then 
+        double[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void randomArrayGeneretedGiven() {
+        //given
+        double[] nums = new double[TEST_SIZE];
+        for (int i = 0; i < TEST_SIZE; i++) {
+            nums[i] = generateRandomWithSeed();
+        }
+
+        //when 
+        sorter.sort(nums);
+
+        //then
+        double[] expected = new double[TEST_SIZE];
+        for (int i = 0; i < TEST_SIZE; i++) {
+            expected[i] = generateRandomWithSeed();
+        }
+        for (int i = 0; i < TEST_SIZE - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                assert false;
+            }
+        }
+
+        if (Arrays.asList(nums).containsAll(Arrays.asList(expected))) {
+            assert true;
+        }
+    }
 }
