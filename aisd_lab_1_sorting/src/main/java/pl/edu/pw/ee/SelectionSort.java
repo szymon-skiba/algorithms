f3fb65be1426a2e8365f4dcf2a7424215291c21b
szymon <skibaexe@gmail.com>
2022-10-19 00:04:26 +0200

    Formatowanie selectionSort.

--- aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/SelectionSort.java
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/SelectionSort.java
@@ -15,18 +15,18 @@ public class SelectionSort implements Sorting {
 
         for (int i = 0; i < n - 1; i++) {
             minValId = i;
-            for(int j = i+1; j<n; j++){
-                if(nums[j]<nums[minValId]){
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[minValId]) {
                     minValId = j;
                 }
             }
-            
-            swap(nums,i,minValId);
+
+            swap(nums, i, minValId);
         }
     }
-    
-    public void swap(double[]nums, int firstId, int secondId){
-        if(firstId != secondId){
+
+    public void swap(double[] nums, int firstId, int secondId) {
+        if (firstId != secondId) {
             double tmp = nums[firstId];
             nums[firstId] = nums[secondId];
             nums[secondId] = tmp;
