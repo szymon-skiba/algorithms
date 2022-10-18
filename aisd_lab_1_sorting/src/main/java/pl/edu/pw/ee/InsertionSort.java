53be56daa9305c7c3350780118554d2ab59aadca
szymon <skibaexe@gmail.com>
2022-10-19 00:03:49 +0200

    Implementacja insertionSort

--- aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/InsertionSort.java
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/InsertionSort.java
@@ -6,8 +6,18 @@ public class InsertionSort implements Sorting {
 
     @Override
     public void sort(double[] nums) {
-           
-        
+        if (nums == null) {
+            throw new IllegalArgumentException("Nums array cannot be null");
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            double tmp = nums[i];
+            int j = i;
+            for (; j > 0 && nums[j - 1] > tmp; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[j] = tmp;
+        }
     }
 
 }
