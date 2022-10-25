dbdc2d5c02b5c48126815e6cf64b840eb6932508
szymon <skibaexe@gmail.com>
2022-10-25 11:38:55 +0200

    Testy stosu i HeapSort.

--- aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/HeapSortTest.java
+++ aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/HeapSortTest.java
@@ -1,5 +1,174 @@
 package pl.edu.pw.ee;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+import pl.edu.pw.ee.services.Sorting;
+
 public class HeapSortTest {
 
+    private Sorting sorter;
+    private Random random;
+    private static final double EPS = 0;
+    private static final long SEED = 173;
+    private static final int TEST_SIZE = 1000;
+
+    private double generateRandomWithSeed() {
+        return (random.nextDouble() * 1000);
+
+    }
+
+    private List<Double> arrayToList(double[] nums) {
+        List<Double> numsAsList = new ArrayList<>();
+
+        for (Double num : nums) {
+            numsAsList.add(num);
+        }
+
+        return numsAsList;
+    }
+
+    @Before
+    public void setUp() {
+        random = new Random(SEED);
+        sorter = new HeapSort();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkIfNullTest() {
+        //given 
+        double[] nums = null;
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        assert false;
+    }
+
+    @Test
+    public void emptyArrayGiven() {
+        //given 
+        double[] nums = {};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        assertEquals(0, nums.length);
+    }
+
+    @Test
+    public void arrayWithCharsGiven() {
+        //given
+        double[] nums = {'d', 'b', 'c', 'a', 1};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        double[] expected = {1, 'a', 'b', 'c', 'd'};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void oneElemntArray() {
+        //given
+        double[] nums = {1};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        double[] expected = {1};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void sortedAscArrayGiven() {
+        //given
+        double[] nums = {1, 2, 3, 4, 5};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        double[] expected = {1, 2, 3, 4, 5};
+        assertArrayEquals(expected, nums, EPS);
+
+    }
+
+    @Test
+    public void sortedDescArrayGiven() {
+        //given
+        double[] nums = {5, 4, 3, 2, 1};
+
+        //when
+        sorter.sort(nums);
+
+        //then
+        double[] expected = {1, 2, 3, 4, 5};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void worstCaseScenarioArrayGiven() {
+        //given 
+        double[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        //when 
+        sorter.sort(nums);
+
+        //then 
+        double[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void bestCaseScenarioArrayGiven() {
+        //given 
+        double[] nums = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        //when 
+        sorter.sort(nums);
+
+        //then 
+        double[] expected = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        assertArrayEquals(expected, nums, EPS);
+    }
+
+    @Test
+    public void randomArrayGeneretedGiven() {
+        //given
+        double[] nums = new double[TEST_SIZE];
+        double[] copy = new double[TEST_SIZE];
+        for (int i = 0; i < TEST_SIZE; i++) {
+            nums[i] = generateRandomWithSeed();
+            copy[i] = nums[i];
+        }
+
+        //when 
+        sorter.sort(nums);
+
+        //then
+        for (int i = 0; i < TEST_SIZE - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                assert false;
+                return;
+            }
+        }
+
+        List<Double> numsCompare = arrayToList(nums);
+        List<Double> copyCompare = arrayToList(copy);
+        if (numsCompare.containsAll(copyCompare)) {
+            assert true;
+            return;
+        }
+
+        assert false;
+    }
 }
--- aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/HeapTest.java
+++ aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/HeapTest.java
@@ -1,5 +1,244 @@
 package pl.edu.pw.ee;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
 public class HeapTest {
 
+    private List<Double> arr;
+    private Random random;
+    private Heap<Double> heap;
+
+    private static final long SEED = 173;
+    private static final double EPS = 0;
+    private static final int TEST_SIZE = 1000;
+
+    private double generateRandomWithSeed() {
+        return random.nextDouble() * 1000;
+
+    }
+
+    @Before
+    public void setUp() {
+        random = new Random(SEED);
+        arr = new ArrayList<>();
+        heap = new Heap<>(arr);
+        heap.build();
+    }
+
+    @Test
+    public void emptyArrayToConstructor() {
+        //given
+        arr = new ArrayList<>();
+
+        //when
+        heap = new Heap<>(arr);
+
+        //then
+        assert true;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullToConstructor() {
+        //given
+        arr = null;
+
+        //when
+        heap = new Heap<>(arr);
+
+        //then
+        assert false;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void putTestNullToHeap() {
+        //given 
+        Double a = null;
+
+        //when
+        heap.put(a);
+
+        //then
+        assert false;
+    }
+
+    @Test
+    public void putTestdoubleDataTypeToHeapThenPopTest() {
+        //given 
+        double a = 1.0;
+
+        //when
+        heap.put(a);
+
+        //then
+        double expected = 1.0;
+        assertEquals(expected, heap.pop(), EPS);
+    }
+
+    @Test
+    public void putTestDoubleObjectToHeapThenPopTest() {
+        //given 
+        Double a = 1.0;
+
+        //when
+        heap.put(a);
+
+        //then
+        double expected = 1.0;
+        assertEquals(expected, heap.pop(), EPS);
+    }
+
+    @Test
+    public void putRandomDoublesToHeapThenPopTest() {
+        //given
+        List<Double> data = new ArrayList<>();
+        for (int i = 0; i < TEST_SIZE; i++) {
+            data.add(generateRandomWithSeed());
+        }
+
+        //when
+        for (Double n : data) {
+            heap.put(n);
+        }
+
+        //then
+        List<Double> result = new ArrayList<>();
+        result.add(heap.pop());
+        for (int i = 1; i < TEST_SIZE; i++) {
+            result.add(heap.pop());
+            if (result.get((i - 1)) <= result.get(i)) {
+                assert false;
+                return;
+            }
+        }
+
+        if (result.containsAll(data)) {
+            assert true;
+            return;
+        }
+
+        assert false;
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void popFromEmptyHeap() {
+        //given
+
+        //when
+        heap.pop();
+
+        //then
+        assert false;
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void popFromNotEmptyHeapButMoreTimesThanElementsInHeap() {
+        //given
+        Double a = 1.0;
+
+        //when
+        heap.put(a);
+        heap.pop();
+        heap.pop();
+
+        //then
+        assert false;
+    }
+
+    @Test
+    public void heapifyWhenHeapHasOneElement_CallLikeBuildLastIteration() {
+        //given
+        Double a = 1.0;
+
+        //when
+        heap.put(a);
+        heap.heapify(0, 1);
+
+        assert true;
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void heapifyWCallStartEqualsSize() {
+        //given
+        Double a = 1.0;
+
+        //when
+        heap.put(a);
+        heap.heapify(1, 1);
+
+        assert false;
+    }
+
+    @Test
+    public void buildWhenRandomValuesGivenToHeap() {
+        //given
+        List<Double> data = new ArrayList<>();
+        for (int i = 0; i < TEST_SIZE; i++) {
+            data.add(generateRandomWithSeed());
+        }
+        heap = new Heap<>(data);
+
+        //when
+        heap.build();
+
+        //then
+        List<Double> result = new ArrayList<>();
+        result.add(heap.pop());
+        for (int i = 1; i < TEST_SIZE; i++) {
+            result.add(heap.pop());
+            if (result.get((i - 1)) < result.get(i)) {
+                assert false;
+                return;
+            }
+        }
+
+        if (result.containsAll(data)) {
+            assert true;
+            return;
+        }
+
+        assert false;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void poppingFromHeapThatWasntBuilded() {
+        //given
+        heap = new Heap<>(arr);
+
+        //when
+        heap.pop();
+
+        //then
+        assert false;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void puttingToHeapThatWasntBuilded() {
+        //given
+        heap = new Heap<>(arr);
+        Double a = 1.0;
+
+        //when
+        heap.put(a);
+
+        //then
+        assert false;
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void heapifyWhenHeapIsEmpty() {
+        //given 
+        int start = 0;
+        int end = 0;
+
+        //when
+        heap.heapify(start, end);
+
+        //then 
+        assert false;
+    }
 }
