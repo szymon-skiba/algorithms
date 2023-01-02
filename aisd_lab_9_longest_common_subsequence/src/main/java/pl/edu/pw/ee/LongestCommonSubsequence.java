6f1f2809397d3e05a524b629d36a7a16c772d0b4
szymon <skibaexe@gmail.com>
2023-01-02 18:24:22 +0100

    LAB_6_FINAL

--- aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/LongestCommonSubsequence.java
+++ aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/LongestCommonSubsequence.java
@@ -1,74 +1,264 @@
 package pl.edu.pw.ee;
 
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+import static java.lang.System.exit;
+
 class LongestCommonSubsequence {
 
     private char[] columns;
     private char[] rows;
-    private int[][] matrix;
+    private int[][] matrixOfDirections;
     private int[][] values;
+    private int[][] isPartOfPath;
 
     private final int top = 3;
     private final int diagonally = 2;
     private final int left = 1;
 
+
     public LongestCommonSubsequence(String firstStr, String secondStr) {
+        if (firstStr == null || secondStr == null) {
+            throw new IllegalArgumentException("Wprowadzone napisy nie moga byc null.");
+        }
 
-        columns = firstStr.toCharArray();
-        rows = secondStr.toCharArray();
+        if (firstStr.isEmpty() || secondStr.isEmpty()) {
+            throw new IllegalArgumentException("Wprowadzone napisy nie moga byc puste.");
+        }
 
-        matrix = new int[secondStr.length() + 1][firstStr.length() + 1];
-        values = new int[secondStr.length() + 1][firstStr.length() + 1];
+        try {
+            columns = firstStr.toCharArray();
+            rows = secondStr.toCharArray();
 
-    }
 
-    public String findLCS() {
-        StringBuilder sb = new StringBuilder();
+            matrixOfDirections = new int[secondStr.length() + 1][firstStr.length() + 1];
+            values = new int[secondStr.length() + 1][firstStr.length() + 1];
+            isPartOfPath = new int[secondStr.length() + 1][firstStr.length() + 1];
+        } catch (OutOfMemoryError e) {
+            throw e;
+        }
+
         for (int i = 0; i < rows.length; i++) {
             for (int j = 0; j < columns.length; j++) {
                 if (rows[i] == columns[j]) {
                     values[i + 1][j + 1] = values[i][j] + 1;
-                    matrix[i + 1][j + 1] = diagonally;
-                    sb.append(rows[i]);
+                    matrixOfDirections[i + 1][j + 1] = diagonally;
                 } else if (values[i][j + 1] >= values[i + 1][j]) {
                     values[i + 1][j + 1] = values[i][j + 1];
-                    matrix[i + 1][j + 1] = top;
+                    matrixOfDirections[i + 1][j + 1] = top;
                 } else {
                     values[i + 1][j + 1] = values[i + 1][j];
-                    matrix[i + 1][j + 1] = left;
+                    matrixOfDirections[i + 1][j + 1] = left;
                 }
             }
         }
 
-        return sb.toString();
+    }
+
+    public String findLCS() {
+        int nRow = rows.length;
+        int nColumn = columns.length;
+
+        StringBuilder result = new StringBuilder();
+
+        int currentCell = matrixOfDirections[nRow][nColumn];
+
+        while (nRow != 0 && nColumn != 0) {
+            isPartOfPath[nRow][nColumn] = 1;
+
+            if (currentCell == diagonally) {
+                result.append(reprChar(rows[nRow - 1]));
+                nRow -= 1;
+                nColumn -= 1;
+            } else if (currentCell == top) {
+                nRow -= 1;
+            } else if (currentCell == left) {
+                nColumn -= 1;
+            } else {
+                throw new IllegalStateException("Nieokreślony kierunek");
+            }
+
+            currentCell = matrixOfDirections[nRow][nColumn];
+        }
+
+        return result.reverse().toString();
+
+    }
+
+    private String reprChar(char c) {
+
+        String result;
+
+        switch (c) {
+
+            case '\0':
+                result = "\\0";
+                break;
+            case '\t':
+                result = ("\\t");
+                break;
+            case '\n':
+                result = ("\\n");
+                break;
+            case '\r':
+                result = ("\\r");
+                break;
+            case '\"':
+                result = ("\\\"");
+                break;
+            case '\\':
+                result = ("\\\\");
+                break;
+            default:
+                result = String.valueOf(c);
+        }
+
+        return result;
     }
 
     public void display() {
-        for (int i = 0; i < rows.length; i++) {
-            if (i == 0) {
-                System.out.print("   ");
-                for (char c : columns) {
-                    System.out.print(" " + c + " ");
-                }
-                System.out.println();
-            }
-            System.out.print(" " + rows[i] + " ");
-            for (int j = 0; j < columns.length; j++) {
-                if (matrix[i + 1][j + 1] == 0) {
-                    System.out.print(' ');
-                } else {
-                    if (matrix[i + 1][j + 1] == left) {
-                        System.out.print('<');
-                    } else if (matrix[i + 1][j + 1] == top) {
-                        System.out.print('^');
-                    } else {
-                        System.out.print("\\");
-                    }
-                }
-                System.out.print(values[i + 1][j + 1] + " ");
+        if (columns.length >= 999999 || rows.length >= 999999) {
+            throw new IllegalStateException("Przekroczono maksymalne parametry wyświetlania");
+        }
+        drawColumnHeaderRow();
+        System.out.println();
+
+        for (int i = 0; i < rows.length + 1; i++) {
+            drawUpperPartOfCell(i);
+            System.out.println();
+            try {
+                drawMiddlePartOfCell(i);
+            } catch (UnsupportedEncodingException e) {
+                System.out.println("Blad podczas tworzenia widoku: " + e.getMessage());
             }
             System.out.println();
+            drawBottomPartOfCell();
+            System.out.println();
+            drawRowLine();
+            System.out.println();
+
         }
 
+    }
+
+    private void drawRowLine() {
+        for (int i = 0; i < columns.length + 2; i++) {
+            if (i == 0) {
+                System.out.print("+-------+");
+            } else {
+                System.out.print("-------+");
+            }
+        }
+    }
+
+    private void drawColumnHeaderRow() {
+        drawRowLine();
+        System.out.println();
+
+        for (int i = 0; i < columns.length + 2; i++) {
+            if (i == 0) {
+                System.out.print("|       |");
+            } else {
+                System.out.print("       |");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < columns.length + 2; i++) {
+            if (i == 0) {
+                System.out.print("|       |");
+            } else if (i == 1) {
+                System.out.print("       |");
+            } else {
+                String str = reprChar(columns[i - 2]);
+                int len = 4 - str.length();
+                System.out.format("%" + len + "s", "");
+                System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+                System.out.print(str + "   |");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < columns.length + 2; i++) {
+            if (i == 0) {
+                System.out.print("|       |");
+            } else {
+                System.out.print("       |");
+            }
+        }
+        System.out.println();
+
+        drawRowLine();
+    }
+
+    private void drawUpperPartOfCell(int row) {
+        System.out.print("|       |");
+
+        for (int i = 0; i < columns.length + 1; i++) {
+            if (isPartOfPath[row][i] == 1) {
+                int direction = matrixOfDirections[row][i];
+                switch (direction) {
+                    case diagonally:
+                        System.out.print("\\   ");
+                        break;
+                    case top:
+                        System.out.print("   ^");
+                        break;
+                    default:
+                        System.out.print("    ");
+                        break;
+                }
+                System.out.print("   |");
+            } else {
+                System.out.print("       |");
+            }
+        }
+    }
+
+    private void drawMiddlePartOfCell(int row) throws UnsupportedEncodingException {
+
+        if (row == 0) {
+            System.out.print("|       |");
+        } else {
+            String str = reprChar(rows[row - 1]);
+            int len = 4 - str.length();
+            System.out.format("|%" + len + "s", "");
+            System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+            System.out.print(str + "   |");
+        }
+
+        for (int i = 0; i < columns.length + 1; i++) {
+
+            int direction = matrixOfDirections[row][i];
+
+            if (direction == left && isPartOfPath[row][i] == 1) {
+                System.out.print("<");
+            } else {
+                System.out.print(" ");
+            }
+
+            int num = values[row][i];
+            int len = 6 - (num == 0 ? 1 : (int) (Math.log10(num) + 1));
+            if (len < 0) {
+                throw new IllegalStateException("Przekroczono maksymalne parametry wyświetlania");
+            }
+            System.out.format("%" + len / 2 + "s", "");
+            System.out.print(num);
+            System.out.format("%" + (len - len / 2) + "s|", "");
+
+        }
+    }
+
+    private void drawBottomPartOfCell() {
+        for (int i = 0; i < columns.length + 2; i++) {
+            if (i == 0) {
+                System.out.print("|       |");
+            } else {
+                System.out.print("       |");
+            }
+        }
     }
 
 }
--- aisd_lab_9_longest_common_subsequence/src/test/java/pl/edu/pw/ee/LongestCommonSubsequenceTest.java
+++ aisd_lab_9_longest_common_subsequence/src/test/java/pl/edu/pw/ee/LongestCommonSubsequenceTest.java
@@ -1,16 +1,211 @@
 package pl.edu.pw.ee;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class LongestCommonSubsequenceTest 
-{
-    
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class LongestCommonSubsequenceTest {
+
+    String redairectedFile = "src/test/resources/redirectedOutput.txt";
+    String pathToTemplateNoPath = "src/test/resources/templateTableNoPath.txt";
+    String pathToTemplateWithPath = "src/test/resources/templateTableWithPath.txt";
+
     @Test
-    public void testMikolaj(){
-        LongestCommonSubsequence lg = new LongestCommonSubsequence("MIKOLAJ","IKONA");
-        System.out.println(lg.findLCS());
+    public void simpleVisualConfirmation_Mikolaj() {
+        //given
+        LongestCommonSubsequence lg = new LongestCommonSubsequence("MIKOLAJ", "IKONA");
+
+        //when
+        String result = lg.findLCS();
+        System.out.println(result);
         lg.display();
+
+        //then
+        assert Objects.equals(result, "IKOA");
     }
+
+    @Test
+    public void visualDisplayWithoutFindLCS() {
+        //given
+        LongestCommonSubsequence lg = new LongestCommonSubsequence("często_z_odkrywaniem", "rzeczy_nie_trzeba\\n_się_spieszyć");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(redairectedFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Blad podczas tworzenia danych testowych" + e.getMessage());
+        }
+        PrintStream tmpOut = new PrintStream(fos);
+        System.setErr(tmpOut);
+        System.setOut(tmpOut);
+
+
+        //when
+        lg.display();
+
+        try {
+            byte[] f1 = Files.readAllBytes(Path.of(pathToTemplateNoPath));
+            byte[] f2 = Files.readAllBytes(Path.of(redairectedFile));
+
+            assertArrayEquals(f1, f2);
+            return;
+        } catch (IOException e) {
+            System.out.println("Blad podczas wczytwania danych testowych" + e.getMessage());
+        }
+
+        //then
+        assert false;
+    }
+
+
+    @Test
+    public void visualDisplayWithFindLCS() {
+        //given
+        LongestCommonSubsequence lg = new LongestCommonSubsequence("często_z_odkrywaniem", "rzeczy_nie_trzeba\n_się_spieszyć");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(redairectedFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Blad podczas tworzenia danych testowych" + e.getMessage());
+        }
+        PrintStream tmpOut = new PrintStream(fos);
+        System.setErr(tmpOut);
+        System.setOut(tmpOut);
+
+
+        //when
+        lg.findLCS();
+        lg.display();
+
+        try {
+            byte[] f1 = Files.readAllBytes(Path.of(pathToTemplateWithPath));
+            byte[] f2 = Files.readAllBytes(Path.of(redairectedFile));
+
+            assertArrayEquals(f1, f2);
+            return;
+        } catch (IOException e) {
+            System.out.println("Blad podczas wczytwania danych testowych" + e.getMessage());
+        }
+
+        //then
+        assert false;
+    }
+
+    @Test(expected = OutOfMemoryError.class)
+    public void tooBigStringsGiven() {
+        //given
+        StringBuilder firstStr = new StringBuilder();
+        StringBuilder secondStr = new StringBuilder();
+        for (int i = 0; i < 999999999; i++) {
+            firstStr.append('A');
+            secondStr.append('A');
+        }
+
+        //when
+        new LongestCommonSubsequence(firstStr.toString(), secondStr.toString());
+
+
+        //then
+        assert false;
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullGiven() {
+        //given
+        LongestCommonSubsequence lg = null;
+        String firstStr = null;
+        String secondStr = null;
+
+        //when
+        lg = new LongestCommonSubsequence(firstStr, secondStr);
+
+        //then
+        assert false;
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void emptyStringsGiven() {
+        //given
+        LongestCommonSubsequence lg = null;
+        String firstStr = "";
+        String secondStr = "";
+
+        //when
+        lg = new LongestCommonSubsequence(firstStr, secondStr);
+
+        //then
+        assert false;
+    }
+
+
+    @Test
+    public void specialCharactersGiven() {
+
+
+        //given
+        LongestCommonSubsequence lg = null;
+        String firstStr = "\0\t \n\r\\";
+        String secondStr = "\0\t \nAAAA\rAAAA\\";
+        lg = new LongestCommonSubsequence(firstStr, secondStr);
+
+
+        //when
+        String expected = "0\\t\\ n\\r\\\\\\";
+        String result = lg.findLCS();
+
+
+        //then
+        assert expected.equals(result);
+    }
+
+
+    @Test
+    public void polishCharactersGiven() {
+
+
+        //given
+        LongestCommonSubsequence lg = null;
+        String firstStr = "ąśćććć";
+        String secondStr = "AAAAAAąśAAAćććć";
+        lg = new LongestCommonSubsequence(firstStr, secondStr);
+
+
+        //when
+        String expected = "ąśćććć";
+        String result = lg.findLCS();
+
+
+        //then
+        assert expected.equals(result);
+    }
+
+
+    @Test()
+    public void multipleSubStringsPossibleGiven_OnlyOneIsLongest() {
+
+
+        //given
+        LongestCommonSubsequence lg = null;
+        String firstStr = "ABCDEFG12345";
+        String secondStr = "ABC15ABBCC12345DEFG12345";
+        lg = new LongestCommonSubsequence(firstStr, secondStr);
+
+
+        //when
+        String expected = "ABCDEFG12345";
+        String result = lg.findLCS();
+
+
+        //then
+        assert expected.equals(result);
+    }
+
+
 }
